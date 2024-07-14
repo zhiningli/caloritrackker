8321a3c5477ffed81349c51a4976a23ec28e540e
Zhining Li <zhiningli.dev@gmail.com>
2024-07-15 00:31:51 +0100

    Added user registration and login functionality

---
+++ src/main/java/com/caloriplanner/calorimeter/clos/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.caloriplanner.calorimeter.clos.repositories;
+
+import com.caloriplanner.calorimeter.clos.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByName(String username);
+    User findByEmail(String email);
+}
