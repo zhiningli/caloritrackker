beea3becec89100d0d78984ff4b1bb5c7bfbc8ef
Zhining Li <zhiningli.dev@gmail.com>
2024-10-21 23:25:25 +0100

    Implemented HTTP GET mapping for health metrics

--- src/main/java/com/caloriplanner/calorimeter/clos/service/HealthMetricsService.java
+++ src/main/java/com/caloriplanner/calorimeter/clos/service/HealthMetricsService.java
@@ -5,5 +5,5 @@ import com.caloriplanner.calorimeter.clos.models.dto.HealthMetricsDto;
 public interface HealthMetricsService {
 
     public void updateHealthMetrics(HealthMetricsDto healthMetricsDto);
-
+    public HealthMetricsDto getHealthMetrics(String userSlug);
 }
--- src/main/java/com/caloriplanner/calorimeter/clos/service/impl/HealthMetricsServiceImpl.java
+++ src/main/java/com/caloriplanner/calorimeter/clos/service/impl/HealthMetricsServiceImpl.java
@@ -2,6 +2,7 @@ package com.caloriplanner.calorimeter.clos.service.impl;
 
 import com.caloriplanner.calorimeter.clos.models.User;
 import com.caloriplanner.calorimeter.clos.models.dto.HealthMetricsDto;
+import com.caloriplanner.calorimeter.clos.models.dto.UserDto;
 import com.caloriplanner.calorimeter.clos.repositories.UserRepository;
 import com.caloriplanner.calorimeter.clos.service.HealthMetricsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,11 @@ public class HealthMetricsServiceImpl implements HealthMetricsService {
 
         user.setHealthMetrics(healthMetricsDto);
         userRepository.save(user);
+    }
+
+    @Override
+    public HealthMetricsDto getHealthMetrics(String userSlug) {
+        User user = userRepository.findBySlug(userSlug);
+        return user.getHealthMetrics();
     }
 }
